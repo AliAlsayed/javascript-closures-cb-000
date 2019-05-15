e9f8173bfb967cb8fffcaba750e10f6796ef943f
Ali Alsayed <a_alsayed87@hotmail.com>
2019-05-15 15:52:52 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,14 +4,14 @@ const fs = require('fs')
 const jsdom = require('mocha-jsdom')
 const path = require('path')
 
-// 
+//
 // describe('index', () => {
-//   
+//
 //   jsdom({
 //     src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 //   })
-//   
-// 
+//
+//
 //   it('runs', () => {
 //     expect(true).toEqual(true)
 //   })
